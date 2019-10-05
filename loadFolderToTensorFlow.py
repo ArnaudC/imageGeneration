f7f9213ceac30f5915dcfe9868ed6a5c9882b808
aorus <a@a.com>
2019-10-05 21:56:12 +0200

    ignore + sorted images

--- loadFolderToTensorFlow.py
+++ loadFolderToTensorFlow.py
@@ -80,10 +80,10 @@ def loadFolderToTensorFlow(folder, image_width, image_height, ratio, resizedFold
 # https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder-in-python
 def removeAllFilesInFolder(folder):
     for the_file in os.listdir(folder):
-    file_path = os.path.join(folder, the_file)
-    try:
-        if os.path.isfile(file_path):
-            os.unlink(file_path)
-        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
-    except Exception as e:
-        print(e)
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
